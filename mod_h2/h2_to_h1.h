f573aaf4112cb10c313b92dd590bc9d639c2d781
Stefan Eissing <stefan.eissing@greenbytes.de>
2015-03-05 15:18:26 +0100

    license headers

--- mod_h2/h2_to_h1.h
+++ mod_h2/h2_to_h1.h
@@ -1,18 +1,17 @@
-/* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+/* Copyright 2015 greenbytes GmbH (https://www.greenbytes.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef __mod_h2__h2_to_h1__
 #define __mod_h2__h2_to_h1__
@@ -20,28 +19,56 @@
 struct h2_mplx;
 typedef struct h2_to_h1 h2_to_h1;
 
+/* Create a converter from a HTTP/2 request to a serialzation in
+ * HTTP/1.1 format. The serialized data will be written onto the
+ * given h2_mplx instance.
+ */
 h2_to_h1 *h2_to_h1_create(apr_pool_t *pool);
 
+/* Destroy the converter and free resources. */
 void h2_to_h1_destroy(h2_to_h1 *to_h1);
 
+/* Start a request with the given method, path and optional authority. For
+ * traceablility reasons, the stream identifier is also given.
+ */
 apr_status_t h2_to_h1_start_request(h2_to_h1 *to_h1, int stream_id, 
                                     const char *method, const char *path,
                                     const char *authority, struct h2_mplx *m);
 
+/* Add a header to the serialization. Only valid to call after start
+ * and before end_headers.
+ */
 apr_status_t h2_to_h1_add_header(h2_to_h1 *to_h1,
                                  const char *name, size_t nlen,
                                  const char *value, size_t vlen,
                                  struct h2_mplx *m);
 
+/* End the request headers.
+ */
 apr_status_t h2_to_h1_end_headers(h2_to_h1 *to_h1, struct h2_mplx *m);
 
+/* Add request body data.
+ */
 apr_status_t h2_to_h1_add_data(h2_to_h1 *to_h1,
                                const char *data, size_t len,
                                struct h2_mplx *m);
 
+/* Flush the converted data onto the h2_mplx instance.
+ */
 apr_status_t h2_to_h1_flush(h2_to_h1 *to_h1, struct h2_mplx *m);
+
+/* Close the request, flushed automatically.
+ */
 apr_status_t h2_to_h1_close(h2_to_h1 *to_h1, struct h2_mplx *m);
 
+/* If the so far serialized data has not been flushed to the h2_mplx
+ * yet, return this data and do not flush it. Also return, if this
+ * data is already the complete request.
+ * This allows short requests to be retrieved directly without going
+ * through the h2_mplx.
+ * Returns NULL, when either nor data is available or if any data
+ * has already been flushed to the h2_mplx for this request.
+ */
 h2_bucket *h2_to_h1_steal_first_data(h2_to_h1 *to_h1, int *peos);
 
 #endif /* defined(__mod_h2__h2_to_h1__) */
--- mod_h2/h2_workers.h
+++ mod_h2/h2_workers.h
@@ -1,12 +1,11 @@
-/* Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+/* Copyright 2015 greenbytes GmbH (https://www.greenbytes.de)
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,18 +27,36 @@ struct h2_task;
 
 typedef struct h2_workers h2_workers;
 
+/* Create a worker pool with the given minimum and maximum number of
+ * threads.
+ */
 h2_workers *h2_workers_create(server_rec *s, apr_pool_t *pool,
                               int min_size, int max_size);
 
+/* Destroy the worker pool and all its threads. 
+ */
 void h2_workers_destroy(h2_workers *workers);
 
+/* Schedule a task for execution.
+ */
 apr_status_t h2_workers_schedule(h2_workers *workers, h2_task *task);
 
+/* If the task is scheduled, but not been started yet, will remove it from 
+ * the schedule and return APR_SUCCESS.
+ * If the task is running and wait != 0, will wait for the task to 
+ * complete. Returns APR_SUCCESS when done.
+ * If the task is running and wait == 0, will return immediately 
+ * with APR_EAGAIN.
+ */
 apr_status_t h2_workers_join(h2_workers *workers, h2_task *task, int wait);
 
+/* Shutdown all activities connection to the session.
+ */
 apr_status_t h2_workers_shutdown(h2_workers *workers,
                                  long session_id);
 
+/* Log some statistics about budy/idle workers etc. 
+ */
 void h2_workers_log_stats(h2_workers *workers);
 
 #endif /* defined(__mod_h2__h2_workers__) */
