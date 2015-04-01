d7be72a41d7ad4dbeabc4dfc414b7156f451739f
Stefan Eissing <stefan.eissing@greenbytes.de>
2015-04-01 16:43:39 +0200

    transformed ALPN httpd-trunk patch to 2.4.12 patch, adapted h2_h2 to double registers now

--- mod_h2/h2_h2.c
+++ mod_h2/h2_h2.c
@@ -46,6 +46,17 @@ apr_size_t h2_protos_len = sizeof(h2_protos)/sizeof(h2_protos[0]);
 APR_DECLARE_OPTIONAL_FN(int, ssl_engine_disable, (conn_rec*));
 APR_DECLARE_OPTIONAL_FN(int, ssl_is_https, (conn_rec*));
 
+typedef int (*ssl_npn_advertise_protos)(conn_rec *connection, 
+                                      apr_array_header_t *protos);
+typedef int (*ssl_npn_proto_negotiated)(conn_rec *connection, 
+                                      const char *proto_name,
+                                      apr_size_t proto_name_len);
+
+APR_DECLARE_OPTIONAL_FN(int, modssl_register_npn, 
+                        (conn_rec *conn,
+                         ssl_npn_advertise_protos advertisefn,
+                         ssl_npn_proto_negotiated negotiatedfn));
+
 typedef int (*ssl_alpn_propose_protos)(conn_rec *connection,
 apr_array_header_t *client_protos, apr_array_header_t *protos);
 
@@ -63,6 +74,9 @@ static int (*opt_ssl_is_https)(conn_rec*);
 static int (*opt_ssl_register_alpn)(conn_rec*,
                                     ssl_alpn_propose_protos,
                                     ssl_alpn_proto_negotiated);
+static int (*opt_ssl_register_npn)(conn_rec*,
+                                    ssl_npn_advertise_protos,
+                                    ssl_npn_proto_negotiated);
 
 static const char *const mod_ssl[] = { "mod_ssl.c", NULL};
 static const char *const more_core[] = { "core.c", NULL};
@@ -93,15 +107,16 @@ apr_status_t h2_h2_init(apr_pool_t *pool, server_rec *s)
     ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, s, "h2_h2, child_init");
     opt_ssl_engine_disable = APR_RETRIEVE_OPTIONAL_FN(ssl_engine_disable);
     opt_ssl_is_https = APR_RETRIEVE_OPTIONAL_FN(ssl_is_https);
+    opt_ssl_register_npn = APR_RETRIEVE_OPTIONAL_FN(modssl_register_npn);
     opt_ssl_register_alpn = APR_RETRIEVE_OPTIONAL_FN(modssl_register_alpn);
     
     if (!opt_ssl_is_https) {
         ap_log_error(APLOG_MARK, APLOG_WARNING, 0, s,
                      "mod_ssl does not seem to be enabled");
     }
-    else if (!opt_ssl_register_alpn) {
+    else if (!opt_ssl_register_alpn && !opt_ssl_register_npn) {
         ap_log_error(APLOG_MARK, APLOG_WARNING, 0, s,
-                     "mod_ssl does not offer ALPN registration");
+                     "mod_ssl does not offer ALPN or NPN registration");
     }
     return APR_SUCCESS;
 }
@@ -128,9 +143,62 @@ static int h2_util_array_index(apr_array_header_t *array, const char *s)
     return -1;
 }
 
+static int h2_h2_npn_advertise(conn_rec *c, apr_array_header_t *protos)
+{
+    h2_config *cfg = h2_config_get(c);
+    if (!h2_config_geti(cfg, H2_CONF_ENABLED)) {
+        return DECLINED;
+    }
+    
+    for (int i = 0; i < h2_protos_len; ++i) {
+        const char *proto = h2_protos[i];
+        ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, c,
+                      "NPN proposing %s from client selection", proto);
+        APR_ARRAY_PUSH(protos, const char*) = proto;
+    }
+    return OK;
+}
+
+static int h2_h2_npn_negotiated(conn_rec *c,
+                                const char *proto_name,
+                                apr_size_t proto_name_len)
+{
+    if (APLOGctrace1(c)) {
+        ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, c,
+                      "NPN negotiated is %s", 
+                      apr_pstrndup(c->pool, proto_name, proto_name_len));
+    }
+    
+    h2_config *cfg = h2_config_get(c);
+    if (!h2_config_geti(cfg, H2_CONF_ENABLED)) {
+        return DECLINED;
+    }
+    
+    if (!h2_ctx_is_session(c) ) {
+        return DECLINED;
+    }
+    
+    if (h2_ctx_is_negotiated(c)) {
+        // called twice? maybe alpn+npn overlap...
+        return DECLINED;
+    }
+    
+    for (int i = 0; i < h2_protos_len; ++i) {
+        const char *proto = h2_protos[i];
+        if (proto_name_len == strlen(proto)
+            && strncmp(proto, proto_name, proto_name_len) == 0) {
+            ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, c, 
+                          "protocol set va NPN to %s", proto);
+            h2_ctx_set_protocol(c, proto);
+            break;
+        }
+    }    
+    return OK;
+}
+
 static int h2_h2_alpn_propose(conn_rec *c,
-                               apr_array_header_t *client_protos,
-                               apr_array_header_t *protos)
+                              apr_array_header_t *client_protos,
+                              apr_array_header_t *protos)
 {
     h2_config *cfg = h2_config_get(c);
     if (!h2_config_geti(cfg, H2_CONF_ENABLED)) {
@@ -175,7 +243,7 @@ static int h2_h2_alpn_negotiated(conn_rec *c,
     }
     
     if (h2_ctx_is_negotiated(c)) {
-        // called twice? should not happen...
+        // called twice? maybe alpn+npn overlap...
         return DECLINED;
     }
     
@@ -215,15 +283,21 @@ int h2_h2_pre_conn(conn_rec* c, void *arg)
             return DECLINED;
         }
         
-        /* Does mod_ssl offer ALPN support? */
-        if (opt_ssl_register_alpn == NULL) {
+        /* Does mod_ssl offer ALPN/NPN support? */
+        if (opt_ssl_register_alpn == NULL && opt_ssl_register_npn == NULL) {
             ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, c,
-                          "h2_h2, pre_connection, no ALPN support in mod_ssl");
+                          "h2_h2, pre_connection, no ALPN/NPN support in mod_ssl");
             return DECLINED;
         }
         
         ctx = h2_ctx_create(c);
-        opt_ssl_register_alpn(c, h2_h2_alpn_propose, h2_h2_alpn_negotiated);
+        if (opt_ssl_register_alpn) {
+            opt_ssl_register_alpn(c, h2_h2_alpn_propose, h2_h2_alpn_negotiated);
+        }
+        if (opt_ssl_register_npn) {
+            opt_ssl_register_npn(c, h2_h2_npn_advertise, h2_h2_npn_negotiated);
+        }
+
         ap_log_cerror(APLOG_MARK, APLOG_TRACE2, 0, c,
                       "h2_h2, pre_connection, ALPN callback registered");
         
