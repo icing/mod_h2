790b457f04bbc914fc815a33669399787ebd012a
Stefan Eissing <stefan.eissing@greenbytes.de>
2015-04-08 11:13:04 +0200

    h2_from_h1 cleanup

--- mod_h2/h2_from_h1.c
+++ mod_h2/h2_from_h1.c
@@ -31,13 +31,6 @@
 #include "h2_task_output.h"
 #include "h2_util.h"
 
-typedef enum {
-    H2_CHNK_SIZE,
-    H2_CHNK_SKIP,
-    H2_CHNK_CRLF,
-    H2_CHNK_DONE,
-} h2_chunk_state_t;
-
 struct h2_from_h1 {
     int stream_id;
     h2_from_h1_state_t state;
@@ -47,8 +40,6 @@ struct h2_from_h1 {
     
     apr_size_t content_length;
     int chunked;
-    h2_chunk_state_t chunk_state;
-    apr_size_t chunk_remain;
     
     const char *status;
     apr_array_header_t *hlines;
@@ -122,8 +113,7 @@ static apr_status_t make_h2_headers(h2_from_h1 *from_h1, request_rec *r)
     }
     from_h1->content_length = from_h1->head->content_length;
     from_h1->chunked = r->chunked;
-    from_h1->chunk_state = H2_CHNK_SIZE;
-
+    
     ap_log_cerror(APLOG_MARK, APLOG_DEBUG, 0, r->connection,
                   "h2_from_h1(%d): converted %d headers, content-length: %d"
                   ", chunked=%d",
@@ -158,110 +148,100 @@ static apr_status_t parse_header(h2_from_h1 *from_h1, ap_filter_t* f,
     return APR_SUCCESS;
 }
 
+static apr_status_t get_line(h2_from_h1 *from_h1, apr_bucket_brigade *bb,
+                             ap_filter_t* f, char *line, apr_size_t len)
+{
+    if (!from_h1->bb) {
+        from_h1->bb = apr_brigade_create(from_h1->pool, f->c->bucket_alloc);
+    }
+    else {
+        apr_brigade_cleanup(from_h1->bb);                
+    }
+    apr_status_t status = apr_brigade_split_line(from_h1->bb, bb, 
+                                                 APR_BLOCK_READ, 
+                                                 HUGE_STRING_LEN);
+    if (status == APR_SUCCESS) {
+        --len;
+        status = apr_brigade_flatten(from_h1->bb, line, &len);
+        if (status == APR_SUCCESS) {
+            /* we assume a non-0 containing line and remove
+             * trailing crlf. */
+            line[len] = '\0';
+            if (len >= 2 && !strcmp(H2_CRLF, line + len - 2)) {
+                len -= 2;
+                line[len] = '\0';
+            }
+            
+            apr_brigade_cleanup(from_h1->bb);
+            ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, f->c,
+                          "h2_from_h1(%d): read line: %s",
+                          from_h1->stream_id, line);
+        }
+    }
+    return status;
+}
+
 apr_status_t h2_from_h1_read_response(h2_from_h1 *from_h1, ap_filter_t* f,
                                       apr_bucket_brigade* bb)
 {
     apr_status_t status = APR_SUCCESS;
-    char buffer[HUGE_STRING_LEN];
-    apr_size_t line_len;
-    
-    if (from_h1->chunked) {
-        ap_remove_output_filter_byhandle(f->r->output_filters, "CHUNK");
-    }
+    char line[HUGE_STRING_LEN];
     
     if ((from_h1->state == H2_RESP_ST_BODY) 
-        || (from_h1->state == H2_RESP_ST_BODY)) {
+        || (from_h1->state == H2_RESP_ST_DONE)) {
+        if (from_h1->chunked) {
+            /* The httpd core HTTP_HEADER filter has or will install the 
+             * "CHUNK" output transcode filter, which appears further down 
+             * the filter chain. We do not want it for HTTP/2.
+             * Once we successfully deinstalled it, this filter has no
+             * further function and we remove it.
+             */
+            status = ap_remove_output_filter_byhandle(f->r->output_filters, 
+                                                      "CHUNK");
+            if (status == APR_SUCCESS) {
+                ap_remove_output_filter(f);
+            }
+        }
+        
         return ap_pass_brigade(f->next, bb);
-    }
-    
-    if (!from_h1->bb) {
-        from_h1->bb = apr_brigade_create(f->c->pool, f->c->bucket_alloc);
     }
     
     while (!APR_BRIGADE_EMPTY(bb) && status == APR_SUCCESS) {
         
         switch (from_h1->state) {
+                
             case H2_RESP_ST_STATUS_LINE:
-                apr_brigade_cleanup(from_h1->bb);                
-                status = apr_brigade_split_line(from_h1->bb, bb, APR_BLOCK_READ, 
-                                                HUGE_STRING_LEN);
-                if (status != APR_SUCCESS) {
-                    return status;
-                }
-                line_len = sizeof(buffer) - 1;
-                status = apr_brigade_flatten(from_h1->bb, buffer, &line_len);
-                if (status != APR_SUCCESS) {
-                    return status;
-                }
-                buffer[line_len] = '\0';
-                apr_brigade_cleanup(from_h1->bb);
-                ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, f->c,
-                              "h2_from_h1(%d): read status: %s",
-                              from_h1->stream_id, buffer);
-                /* instead of parsing, just take it directly */
-                from_h1->status = apr_psprintf(f->c->pool, 
-                                               "%d", f->r->status);
-                from_h1->state = H2_RESP_ST_HEADERS;
-                ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, f->c,
-                              "h2_from_h1(%d): read status %s",
-                              from_h1->stream_id, from_h1->status);
-                break;
-                
             case H2_RESP_ST_HEADERS:
-                apr_brigade_cleanup(from_h1->bb);                
-                status = apr_brigade_split_line(from_h1->bb, bb, APR_BLOCK_READ, 
-                                                HUGE_STRING_LEN);
+                status = get_line(from_h1, bb, f, line, sizeof(line));
                 if (status != APR_SUCCESS) {
                     return status;
                 }
-                line_len = sizeof(buffer) - 1;
-                status = apr_brigade_flatten(from_h1->bb, buffer, &line_len);
-                if (status != APR_SUCCESS) {
-                    return status;
+                if (from_h1->state == H2_RESP_ST_STATUS_LINE) {
+                    /* instead of parsing, just take it directly */
+                    from_h1->status = apr_psprintf(from_h1->pool, 
+                                                   "%d", f->r->status);
+                    from_h1->state = H2_RESP_ST_HEADERS;
                 }
-                buffer[line_len] = '\0';
-                apr_brigade_cleanup(from_h1->bb);                
-                ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, f->c,
-                              "h2_from_h1(%d): read header: %s",
-                              from_h1->stream_id, buffer);
-                
-                if (line_len < 2) {     /* we should have at least a crlf */
-                    return APR_EINVAL;
-                }
-                else if (!strcmp(H2_CRLF, buffer + line_len - 2)) {
-                    line_len -= 2;
-                    buffer[ line_len ] = '\0';
-                }
-                
-                if (line_len == 0) {
+                else if (line[0] == '\0') {
+                    /* end of headers, create the h2_response and
+                     * pass the rest of the brigade down the filter
+                     * chain.
+                     */
                     status = make_h2_headers(from_h1, f->r);
-                    ap_log_cerror(APLOG_MARK, APLOG_TRACE1, 0, f->c,
-                                  "h2_from_h1(%d): made response, state=%d",
-                                  from_h1->stream_id, from_h1->state);
-                    if (0 && from_h1->chunked) {
-                        /* We would like this to work reliably, but we
-                         * cannot be certain how many buckets will be passed
-                         * on with chunking even after removing the filter
-                         * since we do not know what is waiting on our caller
-                         * stack.
-                         */
-                        ap_add_output_filter("H1_TO_H2_UNCHUNK", f->ctx, 
-                                             f->r, f->c);
-                    }
-                    /* ap_remove_output_filter(f); */
                     if (from_h1->bb) {
                         apr_brigade_destroy(from_h1->bb);
                         from_h1->bb = NULL;
                     }
-                    return ap_pass_brigade(f->next, bb);
+                    if (!APR_BRIGADE_EMPTY(bb)) {
+                        return ap_pass_brigade(f->next, bb);
+                    }
                 }
                 else {
-                    status = parse_header(from_h1, f, buffer);
+                    status = parse_header(from_h1, f, line);
                 }
                 break;
                 
             default:
-                /* why are we still here? */
                 return ap_pass_brigade(f->next, bb);
         }
         
