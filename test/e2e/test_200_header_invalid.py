dd05d49abe0f67512ce9ed5ba422d7711effecfb
Stefan Eissing <stefan.eissing@greenbytes.de>
2019-07-18 14:21:12 +0200

    * fixes Timeout vs. KeepAliveTimeout behaviour, see PR 63534 (for trunk now,    mpm event backport to 2.4.x up for vote).  * Fixes stream cleanup when connection throttling is in place.  * Counts stream resets by client on streams initiated by client as cause    for connection throttling.  * Header length checks are now logged similar to HTTP/1.1 protocol handler (thanks @mkaufmann)  * Header length is checked also on the merged value from several header instances    and results in a 431 response.

--- test/e2e/test_200_header_invalid.py
+++ test/e2e/test_200_header_invalid.py
@@ -72,4 +72,41 @@ class TestStore:
             r = TestEnv.curl_post_data(url, "name=x&value=y%%%sz" % hex)
             assert 0 != r["rv"]
     
+    # test header field lengths check, LimitRequestLine (default 8190)
+    def test_200_10(self):
+        url = TestEnv.mkurl("https", "cgi", "/")
+        val = "1234567890" # 10 chars
+        for i in range(3): # make a 10000 char string
+            val = "%s%s%s%s%s%s%s%s%s%s" % (val, val, val, val, val, val, val, val, val, val)
+         # LimitRequestLine 8190 ok, one more char -> 431
+        r = TestEnv.curl_get(url, options=[ "-H", "x: %s" % (val[:8187]) ])
+        assert 200 == r["response"]["status"]
+        r = TestEnv.curl_get(url, options=[ "-H", "x: %sx" % (val[:8188]) ])
+        assert 431 == r["response"]["status"]
+
+    # test header field lengths check, LimitRequestFieldSize (default 8190)
+    def test_200_11(self):
+        url = TestEnv.mkurl("https", "cgi", "/")
+        val = "1234567890" # 10 chars
+        for i in range(3): # make a 10000 char string
+            val = "%s%s%s%s%s%s%s%s%s%s" % (val, val, val, val, val, val, val, val, val, val)
+         # LimitRequestFieldSize 8190 ok, one more char -> 400 in HTTP/1.1
+         # (we send 4000+4188 since they are concatenated by ", "
+        r = TestEnv.curl_get(url, options=[ "-H", "x: %s" % (val[:4000]),  "-H", "x: %s" % (val[:4188]) ])
+        assert 200 == r["response"]["status"]
+        r = TestEnv.curl_get(url, options=[ "--http1.1", "-H", "x: %s" % (val[:4000]),  "-H", "x: %s" % (val[:4189]) ])
+        assert 400 == r["response"]["status"]
+        r = TestEnv.curl_get(url, options=[ "-H", "x: %s" % (val[:4000]),  "-H", "x: %s" % (val[:4191]) ])
+        assert 431 == r["response"]["status"]
+
+    # test header field lengths check, LimitRequestFields (default 100)
+    def test_200_12(self):
+        url = TestEnv.mkurl("https", "cgi", "/")
+        opt=[]
+        for i in range(98): # curl sends 2 headers itself (user-agent and accept)
+            opt += [ "-H", "x: 1" ]
+        r = TestEnv.curl_get(url, options=opt)
+        assert 200 == r["response"]["status"]
+        r = TestEnv.curl_get(url, options=(opt + [ "-H", "y: 2" ]))
+        assert 431 == r["response"]["status"]
 
