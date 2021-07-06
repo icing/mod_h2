9c673b7ad5ea6ecb7e39e9de90f85632eb417b03
Stefan Eissing <stefan.eissing@greenbytes.de>
2021-07-06 13:08:54 +0200

    Removing explicit SNI setting in mod_proxy_http2, since mod_proxy nowadays takes care of that.

--- test/e2e/test_600_h2proxy.py
+++ test/e2e/test_600_h2proxy.py
@@ -8,15 +8,20 @@ class TestStore:
     @pytest.fixture(autouse=True, scope='class')
     def _class_scope(self, env):
         env.setup_data_1k_1m()
-        HttpdConf(env).add_vhost_cgi(h2proxy_self=True).install()
+        conf = HttpdConf(env)
+        conf.add_vhost_cgi(h2proxy_self=True)
+        conf.add("LogLevel proxy_http2:trace2")
+        conf.add("LogLevel proxy:trace2")
+        conf.install()
         assert env.apache_restart() == 0
 
     def test_600_01(self, env):
         url = env.mkurl("https", "cgi", "/h2proxy/hello.py")
         r = env.curl_get(url, 5)
-        assert 200 == r.response["status"]
-        assert "HTTP/2.0" == r.response["json"]["protocol"]
-        assert "on" == r.response["json"]["https"]
-        assert "" != r.response["json"]["ssl_protocol"]
-        assert "on" == r.response["json"]["h2"]
-        assert "off" == r.response["json"]["h2push"]
+        assert r.response["status"] == 200
+        assert r.response["json"]["protocol"] == "HTTP/2.0"
+        assert r.response["json"]["https"] == "on"
+        assert r.response["json"]["ssl_protocol"] != ""
+        assert r.response["json"]["h2"] == "on"
+        assert r.response["json"]["h2push"] == "off"
+        assert r.response["json"]["host"] == f"cgi.{env.http_tld}"
